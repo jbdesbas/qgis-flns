ba57847a8c0ad7736e79b7f913adac3fb9be164a
jb <jb.desbas@gmail.com>
2022-10-16 10:11:14 +0200

    docstring

--- french_locality_name_shortener/plugin_main.py
+++ french_locality_name_shortener/plugin_main.py
@@ -31,6 +31,8 @@ def short_name(string, feature, parent):
     Nom court de la commune
     <h4>Syntax</h4>
     <p><strong>short_name</strong>(<i>name</i>)
+    <h4>Example usage</h4>
+    <p>short_name('Saint-Paul-de-Vence') -> 'St-Paul-de-V.'
     """
     return NameProcessor(string).get_short_name()
 
@@ -41,6 +43,8 @@ def very_short_name(string, feature, parent):
     Nom très court de la commune
     <h4>Syntax</h4>
     <p><strong>very_short_name</strong>(<i>name</i>)
+    <h4>Example usage</h4>
+    <p>very_short_name('Saint-Paul-de-Vence') -> 'St-Paul'
     """
     return NameProcessor(string).get_very_short_name()
 
