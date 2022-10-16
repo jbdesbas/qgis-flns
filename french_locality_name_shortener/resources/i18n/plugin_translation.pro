bb0ed95ef3ad3a0f80171698288648b71519fd92
jb <jb.desbas@gmail.com>
2022-10-16 09:28:22 +0200

    initial commit

---
+++ french_locality_name_shortener/resources/i18n/plugin_translation.pro
@@ -0,0 +1,5 @@
+FORMS = 
+
+SOURCES= ../../plugin_main.py 
+
+TRANSLATIONS = french_locality_name_shortener_en.ts
---
+++ french_locality_name_shortener/toolbelt/__init__.py
@@ -0,0 +1,6 @@
+#! python3  # noqa: E265
+from .log_handler import PlgLogger  # noqa: F401
+from .preferences import PlgOptionsManager  # noqa: F401
+from .translator import PlgTranslator  # noqa: F401
+
+from .one_name_shortener import NameProcessor
